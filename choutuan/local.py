1156b9db3a999efa21469057b5e1eb8fe74d919c
Leohh <948750310@qq.com>
2022-11-20 18:31:20 +0800

    modified settings' format

--- choutuan/local.py
+++ choutuan/local.py
@@ -1,2 +1,11 @@
 SECRET_KEY = 'django-insecure-6vc@m$en!#elzarjc7!=(sd-((lp5aw_i=)ujxupewpi7^cqkm'
 DEBUG = True
+
+
+def DATABASES(base_dir):
+    return {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': base_dir / 'db.sqlite3',
+        }
+    }
